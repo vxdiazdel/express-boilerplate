f0963d4efd50b453d38300fb117d5924389fbe83
Victor Diaz de Leon <vrdiazdel@gmail.com>
2017-05-06 16:36:55 -0700

    Move app config to app.js file

--- server/index.js
+++ server/index.js
@@ -1,26 +1,4 @@
-const express = require('express');
-const indexRoutes = require('./routes');
-const path = require('path');
-const morgan = require('morgan');
-const methodOverride = require('method-override');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const errorHandler = require('errorhandler');
-const app = express();
-
-// All environments
-app.set('port', process.env.PORT || 3000);
-app.use(morgan('dev'));
-app.use(methodOverride());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRoutes);
-
-//Dev environment
-if(app.get('env') === 'development') {
-  app.use(errorHandler());
-}
+const app = require('./app');
 
 app.listen(app.get('port'), () => {
   console.log(`Server running on port ${app.get('port')}...`);
